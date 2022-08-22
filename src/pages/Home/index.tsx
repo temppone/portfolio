f5e0a5bfec68f1f79661df622e6f07cb7bb58f38
temppone <gtvtempone@gmail.com>
2022-08-22 08:37:45 -0300

    feat: react router dom + routes

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styled';
+
+const Home = () => {
+    return (
+        <S.Container>
+            <S.Section>section one</S.Section>
+        </S.Container>
+    );
+};
+
+export default Home;
