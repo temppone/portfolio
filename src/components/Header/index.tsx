ee6ddca510f70af22b16352bcaeb61d1ded0a146
temppone <gtvtempone@gmail.com>
2022-08-22 08:38:27 -0300

    feat: click outside menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import useOnClickOutside from '../../hooks/useClickOutside';
 import LocaleButton from '../LocaleButton';
 import * as S from './styled';
 
@@ -15,10 +16,17 @@ type Header = {
 const Header = ({ items }: Header) => {
     const { t } = useTranslation();
     const [menuIsOpen, setMenuIsOpen] = useState(false);
+    const reference = useRef<HTMLDivElement | null>(null);
+
+    const handleClickOutside = () => {
+        setMenuIsOpen(false);
+    };
+
+    useOnClickOutside(reference, handleClickOutside);
 
     return (
         <S.HeaderContainer isActive={menuIsOpen}>
-            <S.Header>
+            <S.Header ref={reference}>
                 <S.Title onClick={() => setMenuIsOpen(!menuIsOpen)}>
                     {menuIsOpen ? t('Menu.Close') : t('Menu.Header')}
                 </S.Title>
